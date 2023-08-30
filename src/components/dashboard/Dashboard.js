33181fc6e2e38f060c1fdaa516fb45161fe5666e
Erikka Fernando <erikkafernando@gmail.com>
2023-08-31 01:10:39 +0800

    dashboard update

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,38 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Image, Card, Grid, Segment } from "semantic-ui-react";
+import PlaylistModal from "./PlaylistModal";
 
 const Dashboard = ({ token, profile }) => {
   const [playlists, setPLaylists] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [info, setInfo] = useState({});
 
- const croppedImage = {
+  const croppedImage = {
     width: "100%",
     height: "200px",
-    backgroundSize: "cover", /* Crop the image to cover the container */
-    backgroundPosition: "center center" /* Center the image */
+    backgroundSize: "cover" /* Crop the image to cover the container */,
+    backgroundPosition: "center center" /* Center the image */,
   };
 
-  const fixedCardSize ={
-      height: "250px",
-      overflow: "hidden"
+  const fixedCardSize = {
+    height: "250px",
+    overflow: "hidden",
   };
 
-  const shortenDesc = { 
+  const shortenDesc = {
     textOverflow: "ellipsis",
     width: "100%",
-    whiteSpace: "nowrap", 
-    overflow: "hidden" 
-};
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+  };
 
-const headerStyle = {
-  color: "white",
-  background:
-    "linear-gradient(94deg, rgba(119,172,207,1) 1%, rgba(218,181,230,1) 75%)",
-  borderRadius: "5px",
-};
+  const headerStyle = {
+    color: "white",
+    background:
+      "linear-gradient(94deg, rgba(119,172,207,1) 1%, rgba(218,181,230,1) 75%)",
+    borderRadius: "5px",
+  };
 
   useEffect(() => {
-
     // get playlist data
     const getPlaylistData = async () => {
       const response = await axios.get(
@@ -45,12 +47,11 @@ const headerStyle = {
         }
       );
       const { items } = response.data;
-      // console.log("HERE", items);
+      console.log("HERE", items);
       // console.log("prof", profile);
       setPLaylists(items);
     };
 
-    
     getPlaylistData();
   }, [token]);
 
@@ -62,12 +63,11 @@ const headerStyle = {
       const rowItems = playlists.slice(i, i + 5).map((item, index) => (
         <Grid.Column key={index}>
           <Card style={fixedCardSize} onClick={() => onPlaylistClick(item)}>
-            <Image 
-            src={item.images[0].url} 
-            alt={item.name} 
-            size="small" 
-            style={croppedImage}
-            
+            <Image
+              src={item.images[0].url}
+              alt={item.name}
+              size="small"
+              style={croppedImage}
             />
             <Card.Content>
               <Card.Header style={shortenDesc}>{item.name}</Card.Header>
@@ -82,41 +82,43 @@ const headerStyle = {
     return rows;
   };
 
-  const onPlaylistClick = async (item) =>{
+  const onPlaylistClick = async (item) => {
     let tracksHref = item.tracks.href;
+    
 
-    const response = await axios.get(
-      tracksHref,
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const { items } = response.data;
-    console.log("tracks here", items);
-
-  }
+    setInfo({
+      href: tracksHref,
+      name: item.name,
+      desc: item.description,
+      img: item.images[0].url
+    });
+    setIsModalOpen(true);
+  };
 
   return (
     <div className="dashboard">
       <p className="pl-text">Home</p>
       <div className="welcome-div-main">
         <Segment style={headerStyle}>
-          <div className="text-container" >
+          <div className="text-container">
             <p className="greeting">Howdy, {profile.display_name}!</p>
-            <p className="compliment">You got a pretty good taste in music</p>
+            <p className="compliment">You've got good taste in music!</p>
           </div>
         </Segment>
       </div>
 
       <p className="pl-text2">Your top playlists</p>
       <div className="main-playlist-container">
-        <Grid columns={5}>
-          {renderPlaylists()}
-        </Grid>
+        <Grid columns={5}>{renderPlaylists()}</Grid>
       </div>
+
+      {isModalOpen && (
+        <PlaylistModal
+          token={token}
+          info={info}
+          onClose={() => setIsModalOpen(false)}
+        />
+      )}
     </div>
   );
 };
