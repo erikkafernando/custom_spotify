5199a0573f484b5a5a55b6af91e282f3b31a11cb
Erikka Fernando <erikkafernando@gmail.com>
2023-09-04 01:14:13 +0800

    play/pause

--- src/components/music-player/Controls.js
+++ src/components/music-player/Controls.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Button, Icon } from "semantic-ui-react";
 
 export const Controls = ({ token, track, getCurrentTrack }) => {
+  const [playState, setPlayState] = useState(false);
+
+  useEffect(() => {
+    const currentState = async () => {
+      const response = await axios.get("https://api.spotify.com/v1/me/player", {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+
+      setPlayState(response.data.is_playing);
+    };
+
+    currentState();
+  }, [token]);
+
+  const changeState = async () => {
+    const state = playState ? "pause" : "play";
+    await axios.put(
+      `https://api.spotify.com/v1/me/player/${state}`,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+
+    setPlayState(!playState);
+  };
+
   const changeTrack = async (type) => {
     var changeResponse = await axios.post(
       `https://api.spotify.com/v1/me/player/${type}`,
@@ -28,7 +61,21 @@ export const Controls = ({ token, track, getCurrentTrack }) => {
         <Icon name="backward" size="large" />
       </div>
       <div className="play">
-        <Button circular icon="play" size="large" />
+        {playState ? (
+          <Button
+            circular
+            icon="pause"
+            size="large"
+            onClick={() => changeState()}
+          />
+        ) : (
+          <Button
+            circular
+            icon="play"
+            size="large"
+            onClick={() => changeState()}
+          />
+        )}
       </div>
       <div className="next" onClick={() => changeTrack("next")}>
         <Icon name="forward" size="large" />
